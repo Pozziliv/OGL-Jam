1699ce40a0a223737a3e9366c8934372c7362385
Ivan <gureff.al@yandex.ru>
2024-06-15 20:55:58 +0300

    Add setup menu

--- Assets/Source/Scripts/Menu/MenuHandler.cs
+++ Assets/Source/Scripts/Menu/MenuHandler.cs
@@ -1,10 +1,14 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
+    [SerializeField] private PlayerController _playerController;
+    [SerializeField] private GameObject _settingWindow;
+    [SerializeField] private Slider _slider;
     [SerializeField] private int _indexOfScene;
-
+    
     public void ExitGame()
     {
         Application.Quit();
@@ -13,5 +17,20 @@ public class MenuHandler : MonoBehaviour
     public void LoadGame()
     {
         SceneManager.LoadScene(_indexOfScene);
+    }
+
+    public void ShowSettings()
+    {
+        _settingWindow.SetActive(true);
+    }
+
+    public void HideSettings()
+    {
+        _settingWindow.SetActive(false);
+    }
+
+    public void SetNewSensitivity()
+    {
+        _playerController.SetSensitivity(_slider.value);
     }
 }
