b74b4525e3c89094075eaaf7f96e39e094f60b33
Ivan <gureff.al@yandex.ru>
2024-06-17 00:15:53 +0300

    Complete extra material

--- Assets/Source/Scripts/Menu/MenuHandler.cs
+++ Assets/Source/Scripts/Menu/MenuHandler.cs
@@ -1,6 +1,7 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuHandler : MonoBehaviour
 {
@@ -13,7 +14,15 @@ public class MenuHandler : MonoBehaviour
     [Header("Game UI")]
     [SerializeField] private GameObject _activeWindow;
     [SerializeField] private PlayerCursorControl _playerCursorControl;
-    
+    [SerializeField] private bool _isPlayerMainUI;
+    [Space]
+    [SerializeField] private GameObject _imageObject;
+    [SerializeField] private Image _imageHolder;
+    [SerializeField] private TMP_Text _textForPicture;
+    [SerializeField] private string _pictureText;
+
+    private Sprite _selectedPicture;
+
     public void ExitGame()
     {
         Application.Quit();
@@ -42,11 +51,23 @@ public class MenuHandler : MonoBehaviour
     public void HideActiveWindow()
     {
         _activeWindow.SetActive(false);
-        _playerCursorControl.DeactivatePlayerCursor();
+
+        if (_isPlayerMainUI)
+        {
+            _playerCursorControl.DeactivatePlayerCursor();
+        }
     }
 
     public void ShowActiveWindow()
     {
         _activeWindow.SetActive(true);
+    }
+
+    public void ShowSelectedPicture()
+    {
+        _imageObject.SetActive(true);
+        _textForPicture.text = _pictureText;
+        _selectedPicture = gameObject.GetComponent<Image>().sprite;
+        _imageHolder.sprite = _selectedPicture;     
     }
 }
