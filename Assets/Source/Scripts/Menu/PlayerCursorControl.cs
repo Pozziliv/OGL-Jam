a8945c2d70a3f87868b647ab4c1fde00f566fa2b
Sergey L <2006lsd2006@gmail.com>
2024-06-17 11:59:26 +0300

    Fix UI trnsparent

--- Assets/Source/Scripts/Menu/PlayerCursorControl.cs
+++ Assets/Source/Scripts/Menu/PlayerCursorControl.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public class PlayerCursorControl : MonoBehaviour
 {
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private HeadBob _headBob;
 
     public void ActivatePlayerCursor()
     {
         _playerController.enabled = false;
+        _headBob.StopAllCoroutines();
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;    
-        Time.timeScale = 0f;
+        //Time.timeScale = 0f;
     }
 
     public void DeactivatePlayerCursor()
     {
         _playerController.enabled = true;
-        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        //Time.timeScale = 1f;
     }
 }
